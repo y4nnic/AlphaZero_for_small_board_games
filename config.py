dac9ead568a8911487dc7efd2443175dc11a9e0e
Neuhaus, Yannic (yaneu101) <yannic.neuhaus@hhu.de>
2018-11-25 00:07:42 +0100

    updated hyperparameters

--- config.py
+++ config.py
@@ -2,31 +2,30 @@
 
 MCTS = {
     'c_puct': 1.0,
-    'dir_alpha': 0.6,
+    'dir_alpha': 0.5,
     'dir_epsilon': 0.25
 }
 
 OPTIMISATION = {
-    'num_batches': 10,
+    'num_batches': 4,
     'batch_size': 512,
-    'num_epochs': 10
+    'num_epochs': 1
 }
 
 EVALUATION = {
-    'num_games': 100,
+    'num_games': 50,
     'num_simulations': 25,
     'temperature': 0,
-    'show_games': False
 }
 
 SELF_PLAY = {
-    'num_games': 100,
+    'num_games': 50,
     'num_simulations': 25,
     'temperature': 1
 }
 
 NEURAL_NETWORKS = {
-    'learning_rate': 0.002, 
+    'learning_rate': 0.2,
     'regularization_strength': 0.001,
     'num_filters_policy': 512,
     'num_filters_value': 512,
