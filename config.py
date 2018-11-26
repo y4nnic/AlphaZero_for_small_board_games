468db8658587b121806c5b1a41fbd387c7fc2b44
Neuhaus, Yannic (yaneu101) <yannic.neuhaus@hhu.de>
2018-11-26 19:49:58 +0100

    updated config

--- config.py
+++ config.py
@@ -2,35 +2,38 @@
 
 MCTS = {
     'c_puct': 1.0,
-    'dir_alpha': 0.5,
+    'dir_alpha': 0.6,
     'dir_epsilon': 0.25
 }
 
 OPTIMISATION = {
-    'num_batches': 1,
+    'num_batches': 10,
     'batch_size': 512,
     'num_epochs': 10
 }
 
 EVALUATION = {
-    'num_games': 50,
+    'num_games': 100,
     'num_simulations': 25,
     'temperature': 0,
+    'show_games': False
 }
 
 SELF_PLAY = {
-    'num_games': 50,
+    'num_games': 100,
     'num_simulations': 25,
     'temperature': 1
 }
 
 NEURAL_NETWORKS = {
-    'learning_rate': 0.02,
+    'learning_rate': 0.002,
     'regularization_strength': 0.001,
-    'num_filters_policy': 256,
-    'num_filters_value': 256,
-    'num_filters_tower': 256,
-    'num_residual_blocks': 5,
+    'num_filters_policy': 512,
+    'num_filters_value': 512,
+    'num_filters_tower': 512,
+    'num_residual_blocks': 3,
     'kernel_size_tower': 3,
-    'hidden_dim_value': 256
+    'hidden_dim_value': 512
 }
+
+
